6778f54c9f8b9cec3e2006f4b3d53ffafc0f7905
April Darger <april.m.darger@gmail.com>
2020-10-14 15:24:02 -0600

    Working on yup validation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
 import './App.css';
 import './components/Form';
 import validation from './components/FormValidation'
@@ -21,16 +22,28 @@ const initialUsers = [];
 const initalDisabledSubmit = true;
 
 function App() {
+  // State
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initalFormErrors);
+  const [users, setUsers] = useState(initialUsers);
+  const [disabledSubmit, setDisabledSubmit] = useState(initalDisabledSubmit);
 
   // Form Validation
   const inputChange = (inputName, inputValue) => {
     yup
-      .reach(validation, inputName)
-      .validate(inputValue)
-      .then(() => {
-        
+      .reach(validation, inputName) //access FormValidatiaon yup object. param1 = yup validation, param2 = key we want tested.
+      .validate(inputValue) // validate based on user input values
+      .then(() => { // Works - clear the error
+        setFormErrors({
+          ...formErrors, [inputName]: '',
+        });
       })
-  }
+      .catch((err) => { // Fails - provide error message created in FormValidation.js
+        setFormErrors({
+          ...formErrors, [inputName]: err.errors[0], // Validation error from FormValidation.js
+        });
+      });
+  }; 
 
 
   return (
@@ -38,6 +51,6 @@ function App() {
       <Form />
     </div>
   );
-}
+};
 
 export default App;
