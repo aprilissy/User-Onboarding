13d07728a6b778191f8a0e3372ab166821c87e2f
April Darger <april.m.darger@gmail.com>
2020-10-14 14:51:46 -0600

    Setting up form validation.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import './App.css';
 import './components/Form';
+import validation from './components/FormValidation'
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: '',
+  termsOfService: false,
+};
+
+const initalFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  termsOfService: false,
+};
+
+const initialUsers = [];
+const initalDisabledSubmit = true;
 
 function App() {
+
+  // Form Validation
+  const inputChange = (inputName, inputValue) => {
+    yup
+      .reach(validation, inputName)
+      .validate(inputValue)
+      .then(() => {
+        
+      })
+  }
+
+
   return (
     <div className="App">
       <Form />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,56 @@
 import React from 'react';
 
-function Form(props) {
-  const
+export default function Form(props) {
+  const {} = props;
 
-  return(
-    console.log('hello I am here')
+  const onChange = (evt) => {
+
+  };
+
+  return (
+    <form>
+{/* pass in disabled prop or create it above */}
+      <button disabled={disabled}>Submit</button> 
+
+      <label>
+        Name
+        <input
+        // pass in values prop for all inputs below or create above
+          value={values.username}
+          name='username'
+          type='text'
+          onChange={onChange}
+        />
+      </label>
+
+      <label>
+        Email
+        <input
+          value={values.emeil}
+          name='email'
+          type='email'
+          onChange={onChange}
+        />
+      </label>
+
+      <label>
+        Password
+        <input
+          value={values.password}
+          name='password'
+          type='text'
+          onChange={onChange}
+        />
+      </label>
+
+      <label>
+        <input 
+          checked={values.termsOfService}
+          name='termsOfService'
+          type='checkbox'
+          onChange={onChange}
+        />
+      </label>
+    </form>
   );
 };
-
-export default Form;
